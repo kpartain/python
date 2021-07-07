2ea3e0a6c4a17cf4ebf9a869a147dbb78aa039ae
kpartain <karalynnleann@gmail.com>
2021-07-07 11:22:21 -0700

    Finishes pirate game

--- fundamentals/fundamentals/hackathon/classes/ninja.py
+++ fundamentals/fundamentals/hackathon/classes/ninja.py
@@ -3,19 +3,26 @@ class Ninja:
     def __init__( self , name ):
         self.name = name
         self.strength = 10
-        self.speed = 5
+        self.speed = 7
         self.health = 100
     
     def show_stats( self ):
         print(f"Name: {self.name}\nStrength: {self.strength}\nSpeed: {self.speed}\nHealth: {self.health}\n")
 
     def attack( self , pirate ):
-        pirate.health -= self.strength
-        if(pirate.health > 0):
-            print(self.name,"says:",ninjaAttacksText)
-        elif(pirate.health == 0):
-            print(self.name,"says triumphantly:",ninjaWinsText)
-            print(pirate.name,"loses. the victor is", self.name)
+        hitOrMissNumber = random.randint(1,self.speed)
+        if(hitOrMissNumber >= self.speed/2):
+            pirate.health -= self.strength
+            if(pirate.health > 0):
+                print(self.name,"says:",ninjaAttacksText)
+                print(pirate.name,"is down to",pirate.heath," health.")
+                print(pirate.name,"cries out:", pirateWasAttackedText)
+            elif(pirate.health <= 0):
+                print(self.name,"says triumphantly:",ninjaWinsText)
+                print(pirate.name,"loses. the victor is", self.name)
+                print("With his last breath,",pirate.name,"cries out:",pirateDefeatedText)
+        elif(hitOrMissNumber < self.speed/2):
+            print(self.name,"moved too slow! The attack missed!")
         return self
 
 #This is my full attack!
--- fundamentals/fundamentals/hackathon/classes/pirate.py
+++ fundamentals/fundamentals/hackathon/classes/pirate.py
@@ -3,16 +3,26 @@ class Pirate:
     def __init__( self , name ):
         self.name = name
         self.strength = 15
-        self.speed = 3
+        self.speed = 5
         self.health = 100
 
     def show_stats( self ):
         print(f"Name: {self.name}\nStrength: {self.strength}\nSpeed: {self.speed}\nHealth: {self.health}\n")
 
     def attack ( self , ninja ):
-        ninja.health -= self.strength
-        if(ninja.health > 0):
-            print(self.name,"says:",)
+        hitOrMissNumber = random.randint(1,self.speed)
+        if(hitOrMissNumber >= self.speed/2):
+            ninja.health -= self.strength
+            if(ninja.health > 0):
+                print(self.name,"says:",pirateAttacksText)
+                print(ninja.name,"is down to",ninja.heath," health.")
+                print(ninja.name,"cries out:", ninjaWasAttackedText)
+            elif(ninja.health <= 0):
+                print(self.name,"says triumphantly:",pirateWinsText)
+                print(ninja.name,"loses. the victor is", self.name)
+                print("With his last breath,",ninja.name,"cries out:",ninjaDefeatedText)
+        elif(hitOrMissNumber < self.speed/2):
+            print(self.name,"moved too slow! The attack missed!")
         return self
 
 pirateAttacksText = "Avast ye scallywag!"
@@ -21,4 +31,4 @@ ninjaWasAttackedText = "うかつ だ な"
 
 pirateWinsText = "Yo ho yo ho! I'll see ye walk the plank"
 #you show promise!
-ninjaWasDefeatedText: "可能性"
+ninjaDefeatedText: "可能性"
