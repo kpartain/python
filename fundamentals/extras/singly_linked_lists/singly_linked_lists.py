d84340df0665b3ef03b710e6dc9b341b98b86440
kpartain <karalynnleann@gmail.com>
2021-07-07 19:59:26 -0700

    Corrects Indentation

--- fundamentals/extras/singly_linked_lists/singly_linked_lists.py
+++ fundamentals/extras/singly_linked_lists/singly_linked_lists.py
@@ -4,60 +4,59 @@ class SLNode:
         self.next = None
 
 class SList:
-        def __init__(self):
-            self.head = None
-        def add_to_front(self, val):
-            new_node = SLNode(val)
-            new_node.next = self.head
+    def __init__(self):
+        self.head = None
+    def add_to_front(self, val):
+        new_node = SLNode(val)
+        new_node.next = self.head
+        self.head = new_node
+        return self
+
+    def add_to_back(self, val):
+        new_node = SLNode(val)
+        if self.head is None:
             self.head = new_node
-            return self
-
-        def add_to_back(self, val):
-            new_node = SLNode(val)
-            if self.head is None:
-                self.head = new_node
-                return
-            last = self.head
-            while(last.next):
-                last = last.next
-            last.next = new_node
-            return self
-
-        def remove_from_front(self):
+            return
+        last = self.head
+        while(last.next):
+            last = last.next
+        last.next = new_node
+        return self
+    def remove_from_front(self):
             #in progress, optional
-            return self
+        return self
 
-        def remove_from_back(self):
+    def remove_from_back(self):
             #in progress, optional
-            return self
+        return self
 
-        def remove_val(self, val):
-            head_val = self.head
-            if head_val is not None:
-                if head_val.value == val:
-                    self.head = head_val.next
-                    head_val = None
-                    return
-            while head_val is not None:
-                if head_val.data == val:
-                    break
-                prev = head_val
-                head_val = head_val.next
-            if head_val is None:
-                return
-            prev.next = head_val.next
-            head_val = None
-            return self
+    def remove_val(self, val):
+        head_val = self.head
+        if head_val is not None:
+            if head_val.value == val:
+                self.head = head_val.next
+                head_val = None
+            return
+        while head_val is not None:
+            if head_val.data == val:
+                break
+            prev = head_val
+            head_val = head_val.next
+        if head_val is None:
+            return
+        prev.next = head_val.next
+        head_val = None
+        return self
 
-        def insert_at(self, val, n):
+    def insert_at(self, val, n):
             #in progress, optional
-            return self
+        return self
 
-        def print_values(self):
-            printval = self.head
-            while printval is not None:
-                print(printval.value)
-                printval = printval.next
-            return self
+    def print_values(self):
+        printval = self.head
+        while printval is not None:
+            print(printval.value)
+            printval = printval.next
+        return self
 my_list = SList()
 my_list.add_to_front("are").add_to_front("Linked lists").add_to_back("fun!").print_values()
