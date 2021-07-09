edd937b1d84c67a4fe188a8390d1a4e52f0bf22e
kpartain <karalynnleann@gmail.com>
2021-07-09 10:53:14 -0700

    restarting

--- flask/fundamentals/checkerboard/server.py
+++ flask/fundamentals/checkerboard/server.py
@@ -6,15 +6,15 @@ from flask import Flask, render_template  # Import Flask to allow us to create o
 app = Flask(__name__)    # Create a new instance of the Flask class called 'app'
 @app.route('/')          # The '@' decorator associates this route with the function immediately following
 def eight_eight_default():
-    return render_template('index.html', width=8, height=8)
-@app.route('/4')
-def eight_four():
-    return render_template('index.html', width=8, height=4)
-@app.route('/<x>/<y>')
-def x_by_y(x,y):
-    width = int(x)
-    height = int(y)
-    return render_template('index.html', width=width, height=height)
+    return render_template('index.html')
+# @app.route('/4')
+# def eight_four():
+#     return render_template('index.html', width=8, height=4)
+# @app.route('/<x>/<y>')
+# def x_by_y(x,y):
+#     width = int(x)
+#     height = int(y)
+#     return render_template('index.html', width=width, height=height)
 #@app.route('/foo') any other routes    
 #def that_routes_foo():
     #return whatever
