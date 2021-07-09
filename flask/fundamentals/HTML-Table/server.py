88a94cee0c0164f9b14cda74933c71c93da33e14
kpartain <karalynnleann@gmail.com>
2021-07-09 14:31:19 -0700

    Starts table

--- flask/fundamentals/HTML-Table/server.py
+++ flask/fundamentals/HTML-Table/server.py
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def foo():
     return render_template('index.html')
+@app.route('/list')
+def renderList():
+    users_dict = [
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+    return render_template('index.html', users = users_dict)
 #@app.route('/foo') any other routes    
 #def that_routes_foo():
     #return whatever
