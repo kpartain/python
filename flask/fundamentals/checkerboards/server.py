4be30ac8aa150c34c01b7ba0e2e34e9096f9d726
kpartain <karalynnleann@gmail.com>
2021-07-09 11:42:27 -0700

    Finishes checkerboard

--- flask/fundamentals/checkerboards/server.py
+++ flask/fundamentals/checkerboards/server.py
@@ -6,7 +6,15 @@ from flask import Flask, render_template  # Import Flask to allow us to create o
 app = Flask(__name__)    # Create a new instance of the Flask class called 'app'
 @app.route('/')          # The '@' decorator associates this route with the function immediately following
 def hello_world():
-    return render_template('index.html')# Return the string 'Hello World!' as a response
+    return render_template('index.html', width=10, height=10)# Return the string 'Hello World!' as a response
+@app.route('/4')
+def four():
+    return render_template('index.html', width=10, height=4)
+@app.route('/<height>/<width>')
+def h_and_w(height, width):
+    y = int(height)
+    x = int(width)
+    return render_template('index.html', width=x, height=y)
 #@app.route('/foo') any other routes    
 #def that_routes_foo():
     #return whatever
