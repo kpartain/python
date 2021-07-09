8a84c708915adac42b531404986b6ab53f3e0ff1
kpartain <karalynnleann@gmail.com>
2021-07-09 13:02:20 -0700

    Continues checkerboard

--- flask/fundamentals/checkerboards/server.py
+++ flask/fundamentals/checkerboards/server.py
@@ -6,15 +6,18 @@ from flask import Flask, render_template  # Import Flask to allow us to create o
 app = Flask(__name__)    # Create a new instance of the Flask class called 'app'
 @app.route('/')          # The '@' decorator associates this route with the function immediately following
 def hello_world():
-    return render_template('index.html', width=10, height=10)# Return the string 'Hello World!' as a response
+    return render_template('index.html', width=10, height=10, color1="red", color2="black")# Return the string 'Hello World!' as a response
 @app.route('/4')
 def four():
-    return render_template('index.html', width=10, height=4)
+    return render_template('index.html', width=10, height=4, color1="red", color2="black")
 @app.route('/<height>/<width>')
 def h_and_w(height, width):
     y = int(height)
     x = int(width)
-    return render_template('index.html', width=x, height=y)
+    return render_template('index.html', width=x, height=y, color1="red", color2="black")
+@app.route('/<h>/<w>/<colorOne>/<colorTwo>/')
+def h_and_w_with_color(h, w, colorOne, colorTwo):
+    return render_template('index.html', width=int(w), height=int(h), color1=colorOne, color2=colorTwo)
 #@app.route('/foo') any other routes    
 #def that_routes_foo():
     #return whatever
