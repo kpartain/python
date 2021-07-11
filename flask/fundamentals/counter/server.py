01c7addf03f667799e3790113f341bda8ec990c5
kpartain <karalynnleann@gmail.com>
2021-07-11 12:31:01 -0700

    Counter assignment

--- flask/fundamentals/counter/server.py
+++ flask/fundamentals/counter/server.py
@@ -1,28 +1,23 @@
 #pipenv install flask ..... to make pipfile and pipfile.lock
 #pipenv shell ..... to enter into shell
 #python server.py ..... start your server
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, \
+url_for, flash, make_response, session
 app = Flask(__name__)
 app.secret_key = 'thisSecretKey_isJustForDemonstration'
-
+#...
 @app.route('/')
-def foo():
+def visits():
+    if 'visits' in session:
+        session['visits'] = session.get('visits') + 1  # reading and updating session data
+    else:
+        session['visits'] = 1 # setting session data
     return render_template('index.html')
 
-@app.route('/users', methods=['POST'])
-def post_with_session_redirect():
-    print(request.form)
-    key_one = request.form['key_one']
-    session['username'] = request.form['name']
-    return redirect('/show.html')
+@app.route('/destroy_session/')
+def delete_visits():
+    session.pop('visits', None) # delete visits
+    return 'Visits deleted'
 
-@app.route('/show')
-def after_post_redirect():
-    print('Showing the User Info From the Form')
-    print(request.form)
-    #session data is available to the form without handing it to it in return
-    return render_template('show.html')
-
-#this must be below ALL routes
 if __name__=='__main__': 
     app.run(debug=True)
