8fe3740af6557e55af9b9191962367649769f61b
kpartain <karalynnleann@gmail.com>
2021-07-14 09:56:13 -0700

    completes morning algo

---
+++ morning_algos/7.14.js
@@ -0,0 +1,94 @@
+const { thomsonCrossSectionDependencies } = require("mathjs");
+
+class SLNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class SLList {
+    constructor(){
+        this.head = null;
+    }
+
+    addToFront(value) {
+        var newHead = new SLNode(value);
+        newHead.next = this.head;
+        this.head = newHead;
+        return this;
+    }
+
+    addToBack(value) {
+        if(this.head == null) {
+            this.addToFront(value);
+            return this;
+        }
+        var newNode = new SLNode(value);
+        var runner = this.head;
+        while(runner.next != null) {
+            runner = runner.next;
+        }
+        runner.next = newNode;
+        return this;
+    }
+
+    contains(value) {
+        if(this.head == null) {
+            return false;
+        }
+
+        var runner = this.head;
+
+        while(runner != null) {
+            if(runner.value == value) {
+                return true; 
+            } else {
+                runner = runner.next;
+            }
+        }
+
+        return false;
+    }
+
+    removeFront() {
+        if(this.head == null) {
+            return false;
+        }
+        if(this.head.next == null) {
+            this.head = null;
+            return true;
+        }
+        var positionOne = this.head;
+        this.head = positionOne.next;
+        while(positionOne.next != null) {
+            positionOne = positionOne.next;
+        }
+        positionOne.next = null;
+        return true;
+    }
+
+    removeBack() {
+        var runner = this.head;
+        while(runner.next != null) {
+            console.log("before")
+            console.log(runner);
+            runner = runner.next;
+            console.log("after")
+            console.log(runner);
+        }
+        runner.value = null;
+        runner.next = null;
+    }
+}
+
+var mySLL = new SLList()
+mySLL.addToFront(3).addToFront(2).addToFront(1);
+console.log("ORIGINAL")
+console.log(mySLL)
+mySLL.removeFront();
+console.log("REMOVE FRONT")
+console.log(mySLL);
+mySLL.removeBack();
+console.log("REMOVE BACK");
+console.log(mySLL)
