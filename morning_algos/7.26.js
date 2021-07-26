10db2624a2637557f15f426d2c9b56157a62dc0e
Karalynn Partain <78173292+kpartain@users.noreply.github.com>
2021-07-26 09:54:56 -0700

    completes algo

--- morning_algos/7.26.js
+++ morning_algos/7.26.js
@@ -18,16 +18,40 @@
     So findNthFibNum(7) would return 8, because 8 is the 7th number in the fibonacci sequence.
 */
 
+// n * n-1 until n is 1
+//n should never hit 0 as n*0 = 0
 function factorial(num){
-    if(num == 0){
-        return 0
+    if(num == 0 || num == 1){
+        return num 
+    } else {
+        return num * factorial(num - 1);
     }
-    return num == 1 ? num :  num * factorial(num - 1);
+    
 }
-console.log(factorial(5))
+console.log("0:",factorial(0))
+console.log("1:",factorial(1))
+console.log("2:",factorial(2))
+console.log("5:",factorial(5))
 
 
 // BONUS:
+// 0, 1, 1, 2, 3, 5, 8, 13, ... 
+//assumes never negative
 function findNthFibNum(n) {
+    if(n <= 1) {
+        return 0;
+    } else if (n < 4) {
+        return 1;
+    } else {
+        return (findNthFibNum(n - 1) + findNthFibNum(n -2));
+    }
     
 }
+console.log("0, 1, 1, 2, 3, 5, 8, 13, ... ")
+console.log("0:",findNthFibNum(0))
+console.log("1:",findNthFibNum(1))
+console.log("2:",findNthFibNum(2))
+console.log("3:",findNthFibNum(3))
+console.log("4:",findNthFibNum(4))
+console.log("7:",findNthFibNum(7))
+console.log("8:",findNthFibNum(8))
